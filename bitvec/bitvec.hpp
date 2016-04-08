05b9442cf1efea0e27c662535f61f5331639e6fe
CastellaFactory <castellafactory@gmail.com>
2016-04-08 20:53:21 +0900

    wip

---
+++ bitvec/bitvec.hpp
@@ -0,0 +1,13 @@
+#ifndef BITVEC_BITVEC_HPP_INCLUDED
+#define BITVEC_BITVEC_HPP_INCLUDED
+
+class bitvec
+{
+private:
+
+public:
+    bitvec (arguments);
+    ~bitvec ();
+};
+
+#endif // BITVEC_BITVEC_HPP_INCLUDED
