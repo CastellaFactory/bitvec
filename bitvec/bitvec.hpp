84ad4e202f49543af53cbe6af2d15876320f0ea5
CastellaFactory <castellafactory@gmail.com>
2016-04-12 01:04:40 +0900

    wip

--- bitvec/bitvec.hpp
+++ bitvec/bitvec.hpp
@@ -1,13 +1,33 @@
 #ifndef BITVEC_BITVEC_HPP_INCLUDED
 #define BITVEC_BITVEC_HPP_INCLUDED
 
-class bitvec
-{
+#include <cstdint>
+#include <iostream>
+#include <memory>
+
+template <class T,
+          template <class U, class A = std::allocator<U>> class Container>
+class bitvec {
 private:
+    Container<T> blocks;
 
 public:
-    bitvec (arguments);
-    ~bitvec ();
+    // bitvec();
+    //
+    // bitvec(std::size_t size);
+
+    // virtual ~bitvec();
+
+    // void clear();
+    // void set(std::size_t i, T v);
+    void push_back(T v) { blocks.push_back(v); };
+    // T operator[](std::size_t idx) const;
+    void print()
+    {
+        for (auto& v : blocks)
+            std::cout << static_cast<unsigned>(v) << ' ';
+        std::cout << std::endl;
+    }
 };
 
-#endif // BITVEC_BITVEC_HPP_INCLUDED
+#endif  // BITVEC_BITVEC_HPP_INCLUDED
---
+++ test/bitvec_test.cpp
@@ -0,0 +1,12 @@
+#include "../bitvec/bitvec.hpp"
+#include <cstdint>
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    bitvec<std::uint8_t, std::vector> bv;
+    bv.push_back(1);
+    bv.push_back(0);
+    bv.print();
+}
